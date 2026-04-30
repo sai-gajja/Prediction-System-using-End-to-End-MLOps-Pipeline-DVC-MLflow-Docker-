16b43cedb31e548915da4833e19832b2d32fcbd6
sai-gajja <saigajja2004@gmail.com>
2026-04-30 11:17:50 +0530

    Project Stucture ready, env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sai Gajja",
+    author_email="saigajja2004@gmail.com",
+    packages=find_packages()
+)
